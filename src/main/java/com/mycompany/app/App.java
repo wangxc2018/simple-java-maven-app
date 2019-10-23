f41d12f29b81e64ee35dbc647ab663b1ba0a78b2
wangxc2018 <1035371385@qq.com>
2019-10-23 14:57:31 +0800

    add Jenkinsfile

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,6 +6,7 @@ public class App {
         System.out.println("Hello World1!");
         System.out.println("Hello World2!");
         System.out.println("Hello World3!");
+        System.out.println("Hello World4!");
     }
 
 }
