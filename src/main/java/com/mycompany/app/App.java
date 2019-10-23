d67ee89752503bb0232c1ed2a5437625835bf0de
wangxc2018 <1035371385@qq.com>
2019-10-23 17:52:04 +0800

    add Jenkinsfile

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -3,11 +3,7 @@ package com.mycompany.app;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World1!");
-        System.out.println("Hello World2!");
-        System.out.println("Hello World3!");
-        System.out.println("Hello World4!");
-        System.out.println("Hello World5!");
+        System.out.println("Hello World!");
     }
 
 }
